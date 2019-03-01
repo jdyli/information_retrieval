f17e5b53c71be29571f54f468a68db7bcd7668a5
Joost <j.verbraeken@student.tudelft.nl>
2019-03-01 17:41:32 +0100

    Temp commit

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import csv
 import json as js
 import os
 import os.path
+from collections import defaultdict
 
 from whoosh import index
 from whoosh.analysis import StemmingAnalyzer
 from whoosh.fields import Schema, TEXT
+from whoosh.qparser import QueryParser
 
 
 def create_index(archive_name_path, archive_data_path, target_dir):
+    num_lines = sum(1 for line in open(archive_name_path))
     with open(archive_name_path, 'r') as file:
         table_names = file.readlines()
 
@@ -30,11 +33,15 @@ def create_index(archive_name_path, archive_data_path, target_dir):
         os.mkdir(target_dir)
     ix = index.create_in(target_dir, schema)
     not_found = []
+    count = 0
     for location in table_location:
         with open(archive_data_path + "re_tables-" + location[0] + ".json") as f:
             data = js.load(f)
             table_id = "table-" + location[0] + "-" + location[1]
-            print(table_id)
+            print(str(float(count) / float(num_lines)))
+            count += 1
+            if count > 62:
+                break
 
             if table_id in data:
                 data = data[table_id]
@@ -58,17 +65,47 @@ def create_index(archive_name_path, archive_data_path, target_dir):
     return ix
 
 
+def parse_queries():
+    pass
+
+
+def parse_qrels():
+    results = defaultdict(lambda: defaultdict(int))
+    with open("qrels.csv", newline="") as file:
+        reader = csv.reader(file)
+        iter_reader = iter(reader)
+        next(iter_reader)
+        for line in iter_reader:
+            results[line[0]][line[2]] = int(line[3])
+    return results
+
+
 def main():
     archive_name_path = "qrels.csv"
     archive_data_path = "tables/"
     target_dir = "index"
-    index = create_index(archive_name_path, archive_data_path, target_dir)
+    ix = create_index(archive_name_path, archive_data_path, target_dir)
     queries = parse_queries()
+    qrels = parse_qrels()
 
+    queries = [[1, "world interest rates table\n"],
+               [2, "2008 beijing olympics\n"],
+               [3, "fast cars\n"]]
+
+    results = get_results(ix, queries, qrels)
 
     # to do:
     # - incorporting BM 25 scores in index (or TF or DF into the postings of each
     # term)
+
+
+def get_results(ix, queries, qrels):
+    results = defaultdict()
+    for query in queries:
+        with ix.searcher() as searcher:
+            parser = QueryParser("content", ix.schema)
+            myquery = parser.parse(queries[0][1])
+            results[query] = searcher.search(myquery)
 
 
 if __name__ == "__main__":
