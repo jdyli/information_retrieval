77eaaca2e95845d9969ac5ca7ceaed6976df16f6
Joost <j.verbraeken@student.tudelft.nl>
2019-03-05 20:31:09 +0100

    Fix bug

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import math
 import os
 import os.path
 from collections import defaultdict
+from functools import reduce
+from statistics import mean
 
 from whoosh import index
 from whoosh.analysis import StemmingAnalyzer
@@ -106,10 +108,18 @@ def evaluate_results(query, results, qrels):
     return score
 
 
+def get_max_score(list_size):
+    score = 0
+    for i in range(list_size):
+        score += 2.0 / math.log(i + 2, 2)
+    return score
+
+
 def main():
     queries = parse_queries()
     qrels = parse_qrels()
     scores = []
+    normalized_scores = []
     for query in queries:
         ix = create_index(query[0])
         with ix.searcher() as searcher:
@@ -117,10 +127,18 @@ def main():
             score = evaluate_results(query[0], results, qrels)
             print(str(query) + " - " + str(score))
             scores.append((query, score))
+            normalized_score = score / max(0.0000001, get_max_score(len(results.top_n)))
+            normalized_scores.append((query, normalized_score))
+            print(str(query) + " - " + str(normalized_score))
     with open("lexical.csv", "w+") as file:
         csv_writer = csv.writer(file)
         for score in scores:
             csv_writer.writerow([int(score[0][0]), score[1]])
+    with open("lexical_normalized.csv", "w+") as file:
+        csv_writer = csv.writer(file)
+        for score in normalized_scores:
+            csv_writer.writerow([int(score[0][0]), score[1]])
+    print("Average normalized score = " + str(reduce(lambda x, y: (0, x[1] + y[1]), normalized_scores)[1] / len(normalized_scores)))
 
 
 if __name__ == "__main__":
