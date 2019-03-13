2f1398498de99998bc01e18244620f282e1ce221
Jody Liu <26707588+jdyli@users.noreply.github.com>
2019-03-13 21:11:15 +0100

    cleaned up main file

--- main_semantic.py
+++ main_semantic.py
@@ -15,23 +15,31 @@ from util import parse_qrels
 from collections import defaultdict
 from functools import reduce
 
+"""
+The implementation of Word Embeddings with the use of the Gensim library and the pre-trained Word2Vec model of GloVe.
+
+Note: Prior to this code, you first need to create the required index tables and queries. For this task, use the 'semantic_index_creation.py'
+file. Moreover, the file 'glove.6B.300d.txt' is not included in this project due to a large file size. However, this file
+can be found at (https://nlp.stanford.edu/projects/glove/) and can be placed inside the 'semantic_data' folder.
+"""
+
 def load_pretrained_glove_model():
     path = os.path.abspath(os.path.curdir)
     glove_file = datapath(path + '/semantic_data/glove.6B.300d.txt')
     tmp_file = get_tmpfile(path + "/semantic_data/glove.word2vec.txt")
-    #tmp_file = get_tmpfile(path + "/semantic_data/glove.6B.300d.txt.word2vec")
     _ = glove2word2vec(glove_file, tmp_file)
     return KeyedVectors.load_word2vec_format(tmp_file, binary=False)
 
 def get_query_table(query):
     path = os.path.abspath(os.path.curdir)
-    archive_name_path = path + "/semantic_data/index_semantic/" + str(query) + ".txt"
+    #change folder name if you want to use another table collection
+    archive_name_path = path + "/semantic_data/index_semantic/" + str(query) + ".txt" 
     tables = [line.rstrip('\n').split() for line in open(archive_name_path)]
     return tables
 
 def get_queries():
     path = os.path.abspath(os.path.curdir)
-    lines = [line.rstrip('\n').split(",") for line in open(path + "/semantic_data/semantic_queries.txt")]
+    lines = [line.rstrip('\n').split(",") for line in open(path + "/semantic_data/cleaned_queries.txt")]
     lines = [[int(line[0])-1, line[1]] for line in lines]
     return lines
 
@@ -53,26 +61,22 @@ def evaluate_results(query, results, qrels, k):
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    k = 20
+    k = 20 # parameter
     scores = []
     qrels = parse_qrels()
     path = os.path.abspath(os.path.curdir)
-    #tmp_file = get_tmpfile(path + "/semantic_data/gensim_word2vec.txt")
-    #model = KeyedVectors.load_word2vec_format(tmp_file)
     model = load_pretrained_glove_model()
-    termsim_index = WordEmbeddingSimilarityIndex(model.wv) #cos
+    termsim_index = WordEmbeddingSimilarityIndex(model.wv) 
 
-    # compute the top 20 tables based on a query
+    # compute the top k tables based on a query
     list_of_queries = get_queries()
     for query in list_of_queries:
         table_corpus = get_query_table(query[0])
-        dictionary = Dictionary(table_corpus) #cos
-        bow_corpus = [dictionary.doc2bow(table) for table in table_corpus] #cos
-        similarity_matrix = SparseTermSimilarityMatrix(termsim_index, dictionary)  # cos: construct similarity matrix
-        instance = SoftCosineSimilarity(bow_corpus, similarity_matrix, num_best=20) #cos
-        sims = instance[dictionary.doc2bow(query[1].split())] #cos
-        #instance = WmdSimilarity(table_corpus, model, num_best=10) #wmd
-        #sims = instance[query[1]] #wmd
+        dictionary = Dictionary(table_corpus) 
+        bow_corpus = [dictionary.doc2bow(table) for table in table_corpus] 
+        similarity_matrix = SparseTermSimilarityMatrix(termsim_index, dictionary)  
+        instance = SoftCosineSimilarity(bow_corpus, similarity_matrix, num_best=k)
+        sims = instance[dictionary.doc2bow(query[1].split())]
         results = defaultdict(lambda: defaultdict(int))
         print('Query: ', query)
         for i in range(len(sims)):
@@ -85,9 +89,10 @@ if __name__ == "__main__":
         scores.append((query[0], ndcg_score))
 
         # Save results in csv file
-        with open(path + "/semantic_data/semantic_nobody_score_2.csv", "w+") as file:
+        with open(path + "/semantic_data/semantic_nobody_score" + str(k) + ".csv", "w+") as file:
             csv_writer = csv.writer(file)
             for score in scores:
                 csv_writer.writerow([score[0], score[1]])
         print("Average normalized score = " + str(
             reduce(lambda x, y: (0, x[1] + y[1]), scores)[1] / len(scores)))
+[]
