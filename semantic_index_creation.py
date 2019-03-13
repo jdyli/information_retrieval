299fa92a0214b85c98243ecfb380de01f2991652
Jody Liu <26707588+jdyli@users.noreply.github.com>
2019-03-13 21:10:19 +0100

    cleaned up the code, ready to merge

--- semantic_index_creation.py
+++ semantic_index_creation.py
@@ -5,18 +5,21 @@ import csv
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
-
 from util import archive_name_path, target_dir, archive_data_path, parse_queries
+
+"""
+Performs the preprocessing of the raw query data and the table data by using the nltk library. 
+Moreover, after the data is cleaned up the files are saved as text files 
+"""
 
 porter_stemmer = PorterStemmer()
 stop_words = set(stopwords.words('english'))
 
-
 def clean_up(sentence):
     sentence = sentence.lower()
     sentence = word_tokenize(sentence)
-    sentence = [w for w in sentence if not w in stop_words] # remove stopwords
-    sentence = [w for w in sentence if w.isalpha()] # remove numbers and punctuation
+    sentence = [w for w in sentence if not w in stop_words]
+    sentence = [w for w in sentence if w.isalpha()]
     stem_sentence = []
     for word in sentence:
         stem_sentence.append(porter_stemmer.stem(word))
@@ -29,7 +32,6 @@ def create_index(query):
     iter_tables = iter(table_names)
     next(iter_tables)
     queries = [int(x.strip().split(",")[0]) - 1 for x in iter_tables]
-
     iter_tables = iter(table_names)
     next(iter_tables)
     table_names = [x.strip().split(",")[2] for x in iter_tables]
@@ -39,12 +41,10 @@ def create_index(query):
     for table_query, location in zip(queries, table_location):
         if table_query != query:
             continue
-
         table_stem = "table-" + location[0] + "-"
         with open(archive_data_path + "re_tables-" + location[0] + ".json") as f:
             data = json.load(f)
             table_id = table_stem + location[1]
-
             if table_id in data:
                 data = data[table_id]
                 data_pt = data["pgTitle"]
@@ -52,12 +52,10 @@ def create_index(query):
                 data_b = " ".join([" ".join(i) for i in data["data"]])
                 data_sect = data["secondTitle"]
                 data_cap = data["caption"]
-                
                 query_line = "%s %s %s %s %s" % (str(data_b), str(data_pt), str(data_ct), str(data_sect), str(data_cap))
                 cleaned_query_line = clean_up(query_line)
                 query_line = "%s %s" % (str(table_id), 
                                  str(cleaned_query_line))
-
                 filewriter.write(query_line + "\n")
             else:
                 not_found.append(table_id)
@@ -77,7 +75,6 @@ def create_query():
 
 if __name__ == "__main__":
     #create_query() # use when you want to create queries
-                              
     queries = parse_queries() # use when you want to create the tables
     for query in queries:
         create_index(query[0])
